a8380310eac26249d6c023f3e30748fc21715ea5
Jason Biegel <jasonbiegel@gmail.com>
2022-01-17 02:10:31 -0500

    ftp format

--- README.md
+++ README.md
@@ -2,15 +2,27 @@
 
 This script takes a CSV report from NOAA and exports a CSV containing the grow seasons for each year. Specifically, the amount of days between the last Spring and first Fall frosts.
 
-## Generate a report
+## Get Data files
 
-Use the [NOAA Climate Data Online Search](https://www.ncdc.noaa.gov/cdo-web/search) utility. Dataset to `Daily Summaries`, select desired date range and location. Next click the `SEARCH` button. Select the desired location details on the left in the next screen and add to cart. Next checkout but clicking on the cart in the top right corner and clicking `VIEW ALL ITEMS (1)`. In this screen, set the Output Format to `Custom GHCN-Daily CSV`, then click `CONTINUE`. On the next screen enter your email address and click `SUBMIT ORDER`. Then you will have to wait for the system to email you the CSV report. The time you have to wait depends on the size of the report. Small reports take minutes, big reports can take hours.
+### Web Tool
+
+For small amounts of data, use the [NOAA Climate Data Online Search](https://www.ncdc.noaa.gov/cdo-web/search) utility. It has a limit of 10,000 station days per order.
+
+Dataset to `Daily Summaries`, select desired date range and location. Next click the `SEARCH` button. Select the desired location details on the left in the next screen and add to cart. Next checkout but clicking on the cart in the top right corner and clicking `VIEW ALL ITEMS (1)`. In this screen, set the Output Format to `Custom GHCN-Daily CSV`, then click `CONTINUE`. On the next screen enter your email address and click `SUBMIT ORDER`. Then you will have to wait for the system to email you the CSV report. The time you have to wait depends on the size of the report. Small reports take minutes, big reports can take hours.
 
 *NOTE* the input report csv file must be ascending order by date, but this is how it is given from the NOAA online search. Remove any incomplete years from the report file.
 
-## Run
+```
+$ python <python script> <path to CSV report file>
+```
+A new CSV file will be created in the same location as the input file with `grow-seasons` at the end of the file name.
+
+### FTP
+
+For bigger datasets, use the FTP server. With an FTP client, open `ftp://ftp.ncdc.noaa.gov/pub/data/ghcn/daily/`. Find the station's ID with the web tool, then download the file with the station ID name in the `by_station` directory on the FTP server.
+
+The FTP data file is in a different format than the web tool, use the `-f` flag to indicate the FTP format.
 
 ```
-$ python noaa_grow_seasons.py <path to CSV report file>
+$ python -f <python script> <path to CSV report file>
 ```
-A new CSV file will be created in the same location as the input file with `grow_seasons` at the end of the file name.
---
+++ grow_seasons.py
@@ -0,0 +1,91 @@
+import argparse
+import csv
+import os
+from datetime import datetime
+
+parser = argparse.ArgumentParser('NOAA Grow Season Calculator')
+parser.add_argument('-f', '--isftp', action='store_true')
+parser.add_argument('input_file', type=str, help='Input CSV file from NOAA report, see README for details on obtaining report CSV file.')
+
+args = parser.parse_args()
+
+station = None
+name = None
+years = {}
+incomplete_years = {}
+
+def cToF(c):
+    return (c * 9/5) + 32
+
+def parseWebToolOutput():
+    global station, name, years, incomplete_years
+
+    # parse input file
+    with open(args.input_file) as csvfile:
+        reader = csv.DictReader(csvfile)
+        for row in reader:
+            if not station:
+                station = row['STATION']
+            if not name:
+                name = row['NAME']
+
+            date = datetime.strptime(row['DATE'], '%Y-%m-%d')
+
+            if row['TMIN'] == '':
+                incomplete_years[date.year] = None
+            
+            if date.year not in years:
+                years[date.year] = []
+            
+            years[date.year].append(row['TMIN'])
+
+
+def parseFtpOutput():
+    global station, name, years, incomplete_years
+
+    with open(args.input_file) as csvfile:
+        reader = csv.DictReader(csvfile, ['station', 'date', 'type', 'value'])
+        for row in reader:
+            if not station:
+                station = row['station']
+
+            date = datetime.strptime(row['date'], '%Y%m%d')
+            
+            if date.year not in years:
+                years[date.year] = []
+            
+            if row['type'] == 'TMIN':
+                years[date.year].append(cToF(int(row['value']) / 10))
+
+if args.isftp:
+    parseFtpOutput()
+else:
+    parseWebToolOutput()
+
+# output grow season length
+with open('{0}-grow-seasons.csv'.format(os.path.splitext(args.input_file)[0]), 'w') as csvfile:
+    writer = csv.DictWriter(csvfile, fieldnames=['STATION', 'NAME', 'YEAR', 'SEASONDAYS'], quoting=csv.QUOTE_ALL)
+    writer.writeheader()
+
+    for year, days in years.items():
+        if year in incomplete_years:
+            continue
+
+        if len(days) < 365:
+            continue
+
+        last_spring_frost = None
+        first_fall_frost = None
+
+        for i, day_min_temp in enumerate(days):
+            # the 200th day of the year is in the middle of July, hottest time of the year anywhere is the continental USA
+            if i < 200 and int(day_min_temp) < 33:
+                last_spring_frost = i
+            elif i >= 200 and int(day_min_temp) < 33:
+                first_fall_frost = i
+                break
+
+        if not last_spring_frost or not first_fall_frost:
+            continue
+
+        writer.writerow({'STATION': station, 'NAME': name, 'YEAR': year, 'SEASONDAYS': first_fall_frost - last_spring_frost})
